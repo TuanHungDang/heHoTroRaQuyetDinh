c2dbccdb5a05816ca5f6a947b807d203c7aed5db
Tập Làm Dev <dangtuanhung1692003@gmail.com>
2025-05-29 22:39:17 +0700

    Update

---
+++ backend/src/models/authModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    isVerified: { 
+        type: Boolean, 
+        default: false 
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
